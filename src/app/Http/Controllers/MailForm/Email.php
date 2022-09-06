e7f48097eb7c89447108d009744acd94a42801c0
indexpro24@gmail.com <indexpro24@gmail.com>
2022-09-06 21:17:04 +0300

    init

--- src/app/Http/Controllers/MailForm/Email.php
+++ src/app/Http/Controllers/MailForm/Email.php
@@ -10,12 +10,17 @@ use Illuminate\Support\Facades\Mail;
 class Email extends Controller
 {
     public function send(Request $request)
+
     {
         $token = $request->bearerToken();
-
         if($token == env('KEY_GOSTREMONT')){
             Mail::to(env('MAIL_MAIN'))->send(new EmailMail($request));
             Mail::to(env('MAIL_GOSTREMONT'))->send(new EmailMail($request));
         }
+        if($token == env('KEY_BIOHIM')){
+            Mail::to(env('MAIL_MAIN'))->send(new EmailMail($request));
+            Mail::to(env('MAIL_BIOHIM'))->send(new EmailMail($request));
+        }
+
     }
 }
