a0a3c6cd9f7278910053592dddec7b3538dd0fe0
Eugene Che <indexpro24@gmail.com>
2023-07-09 16:45:28 +0300

    c

--- src/resources/views/emails/BioHim/mainForm.blade.php
+++ src/resources/views/emails/BioHim/mainForm.blade.php
@@ -1,6 +1,7 @@
 @component('mail::message')
     # Получен запрос на перекись
     Почта: {{$request->email}}
+    Телефон: {{$request->phone}}
     Вид: {{$request->brand}}
     Тара: {{$request->container}}
     Объём : {{$request->volume}}
