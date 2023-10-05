5ea79a6aa69a37834e44d488cf541ba41bd62191
Eugene Che <indexpro24@gmail.com>
2023-10-05 08:43:29 +0300

    c

--- src/resources/views/emails/ZOV/calculation.blade.php
+++ src/resources/views/emails/ZOV/calculation.blade.php
@@ -1,9 +1,8 @@
 @component('mail::message')
     # Заявка с сайта zov.top на обсчёт
-    {{-- Имя: {{$request->name}}
+    Имя: {{$request->name}}
     Телефон: {{$request->phone}}
-    Адрес: {{$request->address}}
     Почта: {{$request->email}}
-    Комментарий: {{$request->comment}}
-    Проект имеется? - {{$request->project}} --}}
+    Комментарий: {{$request->message}}
+    Промокод - {{$request->promo}}
 @endcomponent
