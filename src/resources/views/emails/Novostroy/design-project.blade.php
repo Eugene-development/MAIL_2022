f5190f393cfc200963e5b935a3e78310491a4ae6
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-02-02 15:45:10 +0300

    c

---
+++ src/resources/views/emails/Novostroy/design-project.blade.php
@@ -0,0 +1,20 @@
+@component('mail::message')
+    # Дизайн Интерьера с сайта Новострой
+    Имя: {{$request->name}}
+    {{-- Телефон: {{$request->phone}}
+    Почта: {{$request->email}}
+    Комментарий: {{$request->comment}}
+
+    ## Заявка на замер
+    Адрес: {{$request->address}}
+
+    ## Запись в салон
+    Метро: {{$request->metro}}
+
+    ## Консультация дизайнера
+    Проект имеется? - {{$request->project}}
+
+    ## Просчёт проекта
+    Проект - {{$request->selectedProject}} --}}
+
+@endcomponent
