2e9a7e6fd3c6de8559648ceea7a31a4d28f8d286
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-02-03 17:57:02 +0300

    с

--- src/resources/views/emails/Novostroy/design-project.blade.php
+++ src/resources/views/emails/Novostroy/design-project.blade.php
@@ -2,6 +2,7 @@
     # Запрос на Дизайн Интерьера (Новострой)
     Имя: {{$request->name}}
     Телефон: {{$request->phone}}
+    Адрес: {{$request->address}}
     Почта: {{$request->email}}
     Комментарий: {{$request->comment}}
 @endcomponent
